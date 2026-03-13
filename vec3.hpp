854a4ee5345bbb9fe24a5584ecaf212c0aaee9a5
kavish <tkavisha@student.42.fr>
2026-03-13 20:36:46 +0100

    added more extern operator function for vec3

--- vec3.hpp
+++ vec3.hpp
@@ -61,6 +61,19 @@ public :
     return (e[0]*e[0] + e[1]*e[1] + e[2]*e[2]);
   }
 
-}
+};
+
+//prototypes
+
+std::ostream& operator<<(std::ostream& out, const vec3& v); 
+vec3 operator+(const vec3& u, const vec3& v);
+vec3 operator-(const vec3& u, const vec3& v); 
+vec3 operator*(const vec3& u, const vec3& v);
+vec3 operator*(double t, const vec3& v); 
+vec3 operator*(const vec3& v, double t); 
+vec3 operator/(const vec3& v, double t);
+double dot(const vec3& u, const vec3& v);
+vec3 cross(const vec3& u, const vec3& v); 
+vec3 unit_vector(const vec3& v); 
 
 #endif
