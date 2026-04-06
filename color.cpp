167353d304d08d8b895bdf6d71912b40801de2f1
kavishan59 <thavarasakavishan@gmail.com>
2026-04-06 06:11:44 +0200

    added anti aliasing using multiple ray around the pixel center

--- color.cpp
+++ color.cpp
@@ -1,5 +1,6 @@
 #include "color.hpp"
 #include <fstream>
+#include "interval.hpp"
 
 
 
@@ -9,9 +10,10 @@ void write_color(std::ofstream& file, const color& pixel_color) {
   auto b = pixel_color.z();
 
   //Translate [0,1] to [0,255]
-  int rbyte = int(255.999 * r);
-  int gbyte = int(255.999 * g);
-  int bbyte = int(255.999 * b);
+  static const interval intensity(0.000,0.999);
+  int rbyte = int(256 * intensity.clamp(r));
+  int gbyte = int(256 * intensity.clamp(g));
+  int bbyte = int(256 * intensity.clamp(b));
   
   //write pixel_color
   file << rbyte << ' ' << gbyte << ' ' << bbyte << '\n';
